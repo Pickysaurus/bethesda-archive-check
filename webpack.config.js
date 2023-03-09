2e47c3e55262616505574cb216e7d3014ad9a6eb
TanninOne <seppleviathan@gmx.de>
2023-03-09 16:20:31 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-let webpack = require('vortex-api/bin/webpack').default;
+const webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('gamebryo-archive-check', __dirname, 4);
+module.exports = webpack('gamebryo-archive-check', __dirname, 5);
 
